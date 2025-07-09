9a569fd2cdba756f9899c75592925157afa6bda8
smehlhoff <stevemehlhoff@gmail.com>
2025-07-08 21:25:43 -0700

    Add sessionk key to admin table

--- purly/user/admin.py
+++ purly/user/admin.py
@@ -5,9 +5,9 @@ from .models import User, UserActivity
 
 
 class UserActivityAdmin(admin.ModelAdmin):
-    list_display = ["user", "ip_address", "user_agent", "created_at"]
+    list_display = ["user", "ip_address", "user_agent", "session_key", "created_at"]
     list_filter = ["created_at"]
-    readonly_fields = ["user", "ip_address", "user_agent", "created_at"]
+    readonly_fields = ["user", "ip_address", "user_agent", "session_key", "created_at"]
 
 
 admin.site.register(User, UserAdmin)
