66d9fb19d783630ae47bf6fe38eb6b8dd513b94d
smehlhoff <stevemehlhoff@gmail.com>
2025-07-12 20:03:25 -0700

    Remove whitespace

--- config/settings/local.py
+++ config/settings/local.py
@@ -8,10 +8,10 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    " http://localhost:5173",
+    "http://localhost:5173",
 ]
 CSRF_TRUSTED_ORIGINS = [
-    " http://localhost:5173",
+    "http://localhost:5173",
 ]
 
 INSTALLED_APPS += [
