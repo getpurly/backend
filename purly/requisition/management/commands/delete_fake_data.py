bab633aab8cf505c96b736c4efdce8c65bd480a9
smehlhoff <stevemehlhoff@gmail.com>
2025-08-24 18:27:13 -0700

    Update fake data delete command

--- purly/requisition/management/commands/delete_fake_data.py
+++ purly/requisition/management/commands/delete_fake_data.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 
 from purly.address.models import Address
+from purly.approval.models import ApprovalGroup
 from purly.project.models import Project
 from purly.requisition.models import Requisition
-from purly.user.models import User
+from purly.user.models import User, UserProfile
 
 
 class Command(BaseCommand):
@@ -11,4 +12,6 @@ class Command(BaseCommand):
         Requisition.objects.all().delete()
         Address.objects.all().delete()
         Project.objects.all().delete()
+        ApprovalGroup.objects.all().delete()
+        UserProfile.objects.all().delete()
         User.objects.all().exclude(id=1).delete()
