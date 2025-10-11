899e9398e14238726ab442dea839d4032e59a68f
smehlhoff <stevemehlhoff@gmail.com>
2025-10-11 11:10:00 -0700

    Add elif statement

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -14,6 +14,8 @@ if [ "${APP_ROLE}" = "web" ]; then
     unset DJANGO_SUPERUSER_PASSWORD
 
     exec gunicorn config.wsgi:application --workers 4 --timeout 60 --bind 0.0.0.0:8000
-else
+
+elif [ "${APP_ROLE}" = "worker" ]; then
     exec celery -A config.celery_app worker -l INFO
+
 fi
