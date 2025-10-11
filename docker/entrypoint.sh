a2aa9708578ca54bb960accae514f586591d51b1
smehlhoff <stevemehlhoff@gmail.com>
2025-10-11 11:06:26 -0700

    Add logic to handle web vs. celery deployments

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-# Wait for database if necessary
-sleep 10
+if [ "${APP_ROLE}" = "web" ]; then
+    # Wait for database if necessary
+    sleep 10
 
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-python manage.py create_initial_superuser
+    python manage.py makemigrations --noinput
+    python manage.py migrate --noinput
+    python manage.py collectstatic --noinput
+    python manage.py create_initial_superuser
 
-unset DJANGO_SUPERUSER_USERNAME
-unset DJANGO_SUPERUSER_EMAIL
-unset DJANGO_SUPERUSER_PASSWORD
+    unset DJANGO_SUPERUSER_USERNAME
+    unset DJANGO_SUPERUSER_EMAIL
+    unset DJANGO_SUPERUSER_PASSWORD
 
-exec gunicorn config.wsgi:application --workers 4 --timeout 60 --bind 0.0.0.0:8000
+    exec gunicorn config.wsgi:application --workers 4 --timeout 60 --bind 0.0.0.0:8000
+else
+    exec celery -A config.celery_app worker -l INFO
+fi
